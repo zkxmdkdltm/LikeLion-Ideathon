6a9f53c4d5ede0b963bd2c83be1b473dd8093958
zkxmdkdltm <zkxmdkdltm@gmail.com>
2022-06-24 17:48:33 +0900

    메인 화면 text 수정

--- FrontEnd/new_/host.js
+++ FrontEnd/new_/host.js
@@ -1,4 +1,4 @@
-let content = "당신이 호스트가 되어, 지인들의 주문을 받을 수 있습니다.\n"
+let content = "\n당신이 호스트가 되어, 지인들의 주문을 받을 수 있습니다.\n"
 + "음식점을 탐색해보세요!\n또는 다른 호스트의 주문에 참여 해 보세요!\n"
 let text = document.querySelector(".text2");
 let i = 0;
