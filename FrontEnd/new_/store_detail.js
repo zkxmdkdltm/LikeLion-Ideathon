3bbb1a11b6b9117050926415acf407394ef943bf
rkdcodus <rcd7275@naver.com>
2022-06-27 04:49:33 +0900

    메뉴 계산 구현 / order_client_end.html 추가

--- FrontEnd/new_/store_detail.js
+++ FrontEnd/new_/store_detail.js
@@ -1,31 +1,47 @@
-var food = document.querySelectorAll('.food');
-var foodindex = 0; 
-const follow_content = document.querySelector(".order_content");
-var sumP = parseInt(document.getElementsByClassName("deliver_price")[0].innerText);
-var deliver_price = parseInt(document.getElementsByClassName("deliver_price")[0].innerText);
-const sumPrice = document.querySelector('.sumP');
-sumPrice.innerText = `${sumP}원`;
 
-function food_click(idx){
-  food[idx].onclick = function(){
-    foodindex = idx;
-    const foodName = document.getElementsByClassName("foodName")[foodindex].innerText;
-    var price = document.getElementsByClassName("price")[foodindex].innerText;
+// 체크된 메뉴 인덱스 찾기
+var checkIndex = [];
+
+function addCheck(){
+  var chs = document.getElementsByName("choose");
+  checkIndex = [];
+  for(var i=0; i<chs.length; i++){
+
+      if(chs[i].checked==true){
+
+         checkIndex.push(i);
+      }
+  }
+  console.log(checkIndex);
+}
+
+// 찾은 인덱스 내 주문 옵션으로 불러오기
+
+let showMyMenu = () => {
+  removeAllchild(); // 초기화
+
+  for(var i=0; i<checkIndex.length;i++){
     
+    const foodName = document.getElementsByClassName("foodName")[checkIndex[i]].innerText;
+    var price = document.getElementsByClassName("price")[checkIndex[i]].innerText;
 
     let Area = document.querySelector(".order_content");
     let orderList = document.createElement('div');
     orderList.setAttribute('class','orderList');
+
     let fn = document.createElement('div');
     fn.setAttribute('class','FN');
+
     let count = document.createElement('div');
     count.setAttribute('class','count');
+
     let amount = document.createElement('input');
     amount.setAttribute('type','number')
-    amount.setAttribute('name','amount');
+    amount.setAttribute('class','amount');    
     amount.setAttribute('value','1');
     amount.setAttribute('min','1');
-    
+    amount.setAttribute('onchange','sumPirce()')
+
     let p = document.createElement('div');
     p.setAttribute('class','P')
 
@@ -34,22 +50,39 @@ function food_click(idx){
     orderList.appendChild(count);
     orderList.appendChild(p);
     count.appendChild(amount);
+
     fn.innerHTML = foodName;
-    p.innerHTML = price
-    
-    sumP = parseInt(price) * 1000 + sumP;
-    sumPrice.innerText = `${sumP}원`;
-    let menu_sum = 0;
-    amount.onchange = function(){
-      menu_sum = parseInt(price) * amount.value * 1000;
-      p.innerHTML = menu_sum;
-      sumP = deliver_price + menu_sum;
-      sumPrice.innerText = `${sumP}원`;
-    };
-    
+    p.innerHTML = `${price}원`
+  }
+
+  sumPirce();
+}
+
+//초기화: order content 안에 생성된 자식 요소 모두 삭제하기 
+let removeAllchild = () => {
+  const Area = document.querySelector(".order_content");
+  while ( Area.hasChildNodes() ){
+     Area.removeChild( Area.firstChild );       
   };
 }
 
-for(var i=0; i<food.length; i++){
-  food_click(i);  
+
+// 내 주문 옵션에 불려온 가격 총합 구하기 + 변경된 수량값 계산하기
+const sumP = document.querySelector(".sumP");
+
+let sumPirce = () => {
+  
+  var sum = 0;
+  var deliver_price = parseInt(document.getElementsByClassName("deliver_price")[0].innerText); 
+
+  for(var i=0; i<checkIndex.length;i++) {
+    const changePrice_area = document.getElementsByClassName("P")[i];
+    var count = parseInt(document.getElementsByClassName("amount")[i].value);
+    var price = parseInt(document.getElementsByClassName("price")[checkIndex[i]].innerText);
+    sum += count * (price*1000) ;
+    changePrice_area.innerText= `${count * price*1000}원`;
+  }
+  sum += deliver_price;
+  
+  sumP.innerText = `${sum}원`;
 }
