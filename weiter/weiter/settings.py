4654b35c4345d6316ffd072551661fe69f9a2beb
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2022-06-28 00:12:21 +0900

    Update settings.py

--- weiter/weiter/settings.py
+++ weiter/weiter/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
