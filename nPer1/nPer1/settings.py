2919da1f47884458a6bcbbfb4c54d1c7b47253ef
yuujinkim <yuuuuu01@naver.com>
2022-06-30 05:42:59 +0900

    image

--- nPer1/nPer1/settings.py
+++ nPer1/nPer1/settings.py
@@ -151,3 +151,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
