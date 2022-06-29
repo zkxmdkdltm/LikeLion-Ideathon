18a15b06726c6ac4deb269ce0e375ef649dc5d49
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2022-06-29 22:36:00 +0900

    Delete my_settings.py

--- BackEnd/nPer1/nPer1/my_settings.py
+++
@@ -1,9 +0,0 @@
-EMAIL = {
-'EMAIL_BACKEND' : 'django.core.mail.backends.smtp.EmailBackend',
-'EMAIL_USE_TLS' : True,
-'EMAIL_PORT' : 587,
-'EMAIL_HOST' : 'smtp.gmail.com',   
-'EMAIL_HOST_USER' : 'e_hyeok9@kookmin.ac.kr',                    
-'EMAIL_HOST_PASSWORD' : 'young6415@',
-'REDIRECT_PAGE' : 'http://127.0.0.1:8000/accounts/register'
-}
